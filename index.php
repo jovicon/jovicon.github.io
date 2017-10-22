e7d9beb3f74faeb4d42f4d1aae668fc7239dcbd8
jovicon <jocontrera16@gmail.com>
2017-10-22 14:59:37 -0300

    begining test.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jose Contreras | Developer</title>
+
+    <!-- resources: css-->
+    <link rel="stylesheet" href="rsrc/wow-master/css/libs/animate.css">
+
+    <!-- resources: js -->
+    <script type="text/javascript" src="rsrc/wow-master/dist/wow.min.js"></script>
+
+    <!-- my styles -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+  </head>
+
+  <body>
+
+    <?php include 'php/templates/building.php'; ?>
+
+  </body>
+</html>
---
+++ php/templates/building.php
@@ -0,0 +1,23 @@
+<?php
+
+  $building = '';
+  $building = '
+                <section class="full-screen">
+                  <div class="hero-img"></div>
+
+                  <div class="fixed-layer">
+
+                    <p>Hi! <br> My name is Jose Contreras.</p>
+                    <p>I\'m happy about your visit but actually, I\'m developing my website.</p>
+                    <p>So, You can get in touch here: </p>
+                    <a href="mailto:jocontrera16@gmail.com">Jocontrera16@gmail.com</a>
+                    <p> or +56972216367 </p>
+
+                  </div>
+
+                </section>
+              ';
+
+  echo $building;
+
+ ?>
